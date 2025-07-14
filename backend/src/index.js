bf313c66271e4c94c55ab6551ed92bada1275ca0
Abhishek <144004785+abhikc2811@users.noreply.github.com>
2025-07-14 11:36:40 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-application-1-07hy.onrender.com",
     credentials: true,
   })
 );
