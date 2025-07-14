a0f7af87caef47d20059971f64e9e5a28da69a8f
Abhishek <144004785+abhikc2811@users.noreply.github.com>
2025-07-14 12:32:34 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client";
 
-const BASE_URL="http://localhost:5001";
+const BASE_URL="https://chat-application-pp2j.onrender.com";
 
 export const useAuthStore = create((set,get) => ({
   authUser: null,
@@ -89,6 +89,7 @@ export const useAuthStore = create((set,get) => ({
       query:{
         userId:authUser._id,
       },
+      withCredentials: true,
     })
     socket.connect();
     set({socket:socket});
