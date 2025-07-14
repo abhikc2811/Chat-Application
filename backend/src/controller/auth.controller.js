f9ed190025ccb3edb0bc9eb694419da64c83a167
Abhishek <144004785+abhikc2811@users.noreply.github.com>
2025-07-14 13:25:40 +0530

    Update auth.controller.js

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -153,7 +153,12 @@ export const resetPassword = async (req, res) => {
 
 export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
+    res.cookie("jwt", "", {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      expires: new Date(0),
+    });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
